75be3fc9f6f68346af68a09a6593a56378370454
suriyasg <suriyasg@gmail.com>
2024-09-09 09:16:24 +0530

    Create coordinators page

---
+++ src/services/coordinatorsService.ts
@@ -0,0 +1,65 @@
+import axiosInstance from "../axiosConfig";
+
+export const addCoordinator = async (
+  name: string,
+  district_id: number,
+  telephone_no: string,
+) => {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axiosInstance.post("/coordinator/add", {
+      name,
+      district_id,
+      telephone_no,
+    }, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log("response", response);
+    return true;
+  } catch (error: any) {
+    console.error("Error Adding Coordinator:", error);
+    throw error.response.data.error;
+  }
+};
+
+export const updateCoordinator = async (
+  id: number,
+  name: string,
+  telephone_no: string,
+) => {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axiosInstance.put("/coordinator/" + id, {
+      name,
+      telephone_no,
+    }, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log("response", response);
+    return true;
+  } catch (error: any) {
+    console.error("Error Updating Coordinator:", error);
+    throw error.response.data.error;
+  }
+};
+
+export const deleteCoordinator = async (id: number) => {
+  try {
+    console.log(id);
+    const token = localStorage.getItem("token");
+    const response = await axiosInstance.delete("/coordinator/" + id, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log("response", response);
+    return true;
+  } catch (error: any) {
+    console.error("Error Deleting Coordinator:", error);
+    throw error.response.data.error;
+  }
+};
