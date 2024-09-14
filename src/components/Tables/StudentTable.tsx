58a6a483bbca0c265f8abf0500bc398132648093
unknown <prathhp231@gmail.com>
2024-09-14 09:48:32 +0530

    fixed rank dist id

--- src/components/Tables/StudentTable.tsx
+++ src/components/Tables/StudentTable.tsx
@@ -435,7 +435,7 @@ const StudentTable = ({studentData,itemsPerPage,nameSearchKey,/* streamSearchKey
             </label>
             <select
               value={rankDistrictId}
-              onChange={(e) => setExamDistrictId(Number(e.target.value))}
+              onChange={(e) => setRankDistrictId(Number(e.target.value))}
               className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
             >
               <option value="" disabled>Select Rank District</option>
