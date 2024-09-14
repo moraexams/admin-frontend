c9527c4137e091ca0d26ddee024d9f447d9f5d72
unknown <prathhp231@gmail.com>
2024-09-14 09:38:08 +0530

    Options to select Districts, Centers

--- src/components/Tables/StudentTable.tsx
+++ src/components/Tables/StudentTable.tsx
@@ -466,7 +466,7 @@ const StudentTable = ({studentData,itemsPerPage,nameSearchKey,/* streamSearchKey
           </div>
           <div className="mb-4.5">
             <label className="mb-2.5 block text-black dark:text-white">
-              Center Id <span className="text-meta-1">*</span>
+              Center <span className="text-meta-1">*</span>
             </label>
             <select
               value={centreId}
