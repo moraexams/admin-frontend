d4b8055785597c149d6da747961e5022cbc7e91a
Sahithyan <sahithyan2701@gmail.com>
2025-04-06 19:19:49 +0530

    chore: show admin users differently

--- src/components/Tables/UsersTable.tsx
+++ src/components/Tables/UsersTable.tsx
@@ -68,7 +68,22 @@ const UsersTable = ({ userData, itemsPerPage }: { userData: User[], itemsPerPage
                     </td>
                     <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
                       <p className="text-black dark:text-white">
-                        {user.username}
+                        {user.username}{" "}
+                        {user.role === "admin" ? (
+                          <svg
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            className="inline-block text-yellow-300 icon icon-tabler icons-tabler-outline icon-tabler-crown"
+                          >
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M12 6l4 6l5 -4l-2 10h-14l-2 -10l5 4z" />
+                          </svg>
+                        ) : null}
                       </p>
                     </td>
                     <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
