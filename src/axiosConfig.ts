d9ebd19bca009222b2cc66d9720426acacc12fbe
Sahithyan <sahithyan2701@gmail.com>
2025-04-09 11:30:28 +0530

    chore: include auth token in interceptor

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,33 +1,19 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.env.VITE_BACKEND_URL, // Update this if your API URL is different
+	baseURL: import.meta.env.VITE_BACKEND_URL,
 	headers: {
 		"Content-Type": "application/json",
 	},
 });
 
-// Optional: Add request and response interceptors
-axiosInstance.interceptors.request.use(
-	(config) => {
-		// Do something before the request is sent
-		return config;
-	},
-	(error) => {
-		// Do something with request error
-		return Promise.reject(error);
-	},
-);
+axiosInstance.interceptors.request.use((config) => {
+	const token = localStorage.getItem("token");
+	if (token) {
+		config.headers.Authorization = `Bearer ${token}`;
+	}
 
-axiosInstance.interceptors.response.use(
-	(response) => {
-		// Do something with response data
-		return response;
-	},
-	(error) => {
-		// Do something with response error
-		return Promise.reject(error);
-	},
-);
+	return config;
+});
 
 export default axiosInstance;
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,12 +2,7 @@ import axiosInstance from "../axiosConfig";
 
 export const getUsers = async () => {
 	try {
-		const token = localStorage.getItem("token");
-		const response = await axiosInstance.get("/user", {
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		});
+		const response = await axiosInstance.get("/user");
 		return response.data;
 	} catch (error) {
 		console.error("Error fetching users:", error);
@@ -17,12 +12,7 @@ export const getUsers = async () => {
 
 export const approveUser = async (id: number) => {
 	try {
-		const token = localStorage.getItem("token");
-		const response = await axiosInstance.put(`/user/approve/${id}`, null, {
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		});
+		const response = await axiosInstance.put(`/user/approve/${id}`);
 		return response.data;
 	} catch (error) {
 		console.error("Error approving user:", error);
