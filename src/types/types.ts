b3a8bae76caa8b8610008d3a4a335a7a6eab0658
Sahithyan <sahithyan2701@gmail.com>
2025-05-10 01:12:37 +0530

    chore: remove unwanted types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,30 +1,3 @@
-export interface Patient {
-	id?: number;
-	name: string;
-	contact_number: string;
-	email: string;
-	address: string;
-	date_of_birth: string;
-}
-
-export interface Doctor {
-	id?: number;
-	name: string;
-	specialty: string;
-	contact_number: string;
-	email: string;
-	availability?: string[];
-}
-
-export interface Appointment {
-	id?: number;
-	patient: Patient;
-	doctor: Doctor;
-	appointment_date: string;
-	appointment_time: string;
-	status: string;
-}
-
 export interface User {
 	id?: number;
 	name: string;
