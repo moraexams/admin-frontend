bd99db01a19fbaea79d714b96ad080b416ef8389
KRakeesh04 <kanesrakeshan@gmail.com>
2025-07-23 11:49:24 +0530

    chore: add attendanceSheet service

---
+++ src/services/attendanceSheetService.ts
@@ -0,0 +1,34 @@
+import { AxiosError } from "axios";
+import axiosInstance from "../axiosConfig";
+
+
+export const generateAttendanceSheetPDFs = async () => {
+    try {
+        const response = await axiosInstance.get("/attendance-sheet/generate");
+        return response.data
+    } catch (error) {
+        console.log(error);
+        if (error instanceof AxiosError) {
+            if (error.response?.data) {
+                throw new Error(error.response?.data.message);
+            }
+        }
+        throw "An error occurred while generating attendance sheets";
+    }
+}
+
+
+export const downloadAttendanceSheets = async () => {
+    try {
+        const response = await axiosInstance.get("/attendance-sheet/download");
+        return response.data
+    } catch (error) {
+        console.log(error);
+        if (error instanceof AxiosError) {
+            if (error.response?.data) {
+                throw new Error(error.response?.data.message);
+            }
+        }
+        throw "An error occurred while download attendance sheet zip file";
+    }
+}
