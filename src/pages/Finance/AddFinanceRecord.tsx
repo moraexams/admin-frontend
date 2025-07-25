197a724ab60795e57f1c0d16fc884176c34f74d4
Sahithyan <sahithyan2701@gmail.com>
2025-07-25 23:46:59 +0530

    fix: district is not required for add transaction

--- src/pages/Finance/AddFinanceRecord.tsx
+++ src/pages/Finance/AddFinanceRecord.tsx
@@ -40,6 +40,7 @@ const AddFinanceRecord: React.FC = () => {
 	} = useForm<FinanceFormData>({
 		resolver: zodResolver(financeSchema),
 		defaultValues: {
+			district: 0,
 			transactionType: "expense",
 			createdAt: getCurrentDateTime(),
 			paymentAccount: "cash",
@@ -61,6 +62,7 @@ const AddFinanceRecord: React.FC = () => {
 			amount: 0,
 			createdAt: getCurrentDateTime(),
 			paymentAccount: watch("paymentAccount"),
+			district: 0,
 		});
 		setAmountInput("0");
 	};
@@ -207,7 +209,7 @@ const AddFinanceRecord: React.FC = () => {
 							id="district"
 							className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
 						>
-							<option value="">Select district</option>
+							<option value={0}>None</option>
 							{districts.map((district) => (
 								<option key={district.id} value={district.id}>
 									{capitalize(district.name)}
