e6a89866e8ba05d592f33d16b1a6959564888282
Sahithyan <sahithyan2701@gmail.com>
2025-09-04 10:03:32 +0530

    progress

--- src/pages/DistrictsOverview.tsx
+++ src/pages/DistrictsOverview.tsx
@@ -6,9 +6,10 @@ import toast from "react-hot-toast";
 interface DistrictItem {
 	id: number;
 	name: string;
-	exam_centres: Array<unknown>;
-
-
+	exam_centres: Array<{
+		id: number;
+		name: string;
+	}>;
 }
 
 export default function DistrictsOverview() {
@@ -38,14 +39,29 @@ export default function DistrictsOverview() {
 					<div>Loading...</div>
 				) : (
 					data.map((d) => {
+						if (d.exam_centres.length === 0) return null;
 						return (
-							<section>
-								<div className="flex gap-2">
-									<h2 key={d.id} className="text-xl font-semibold mb-4">
+							<section className="my-4">
+								<div className="flex gap-2 items-center mb-2">
+									<h2 key={d.id} className="text-xl font-semibold">
 										{d.name}
 									</h2>
 
 									<span>{d.exam_centres.length} Centres</span>
+								</div>
+
+								<div className="grid grid-cols-[auto_1fr] overview-table">
+									<div className="contents">
+										<span className="font-bold">Id</span>
+										<span className="font-bold">Name</span>
+									</div>
+
+									{d.exam_centres.map((centre) => (
+										<div key={centre.id} className="contents">
+											<span className="py-1">{centre.id}</span>
+											<span>{centre.name}</span>
+										</div>
+									))}
 								</div>
 							</section>
 						);
