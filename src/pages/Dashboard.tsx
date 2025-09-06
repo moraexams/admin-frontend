078491d579e8dcafbf44f9216fb92539270922c5
Sahithyan <sahithyan2701@gmail.com>
2025-09-06 23:02:39 +0530

    chore: simplify

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -187,126 +187,121 @@ const Dashboard: React.FC = () => {
 							</tbody>
 						</table>
 					</div>
-					<div className="rounded-xs border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1 mt-4 xl:mt-6 2xl:mt-7.5">
-						<div>
-							<div className="mb-6">
-								<h2 className="text-xl font-semibold text-black dark:text-white">
-									Centre Wise Stats
-								</h2>
+					<div>
+						<div className="mt-8 mb-6">
+							<h2 className="text-xl font-semibold text-black dark:text-white">
+								Centre Wise Stats
+							</h2>
+						</div>
+						<div className="flex flex-wrap gap-x-4">
+							<div className="mb-5.5">
+								<select
+									className="rounded border border-stroke bg-white py-3 px-4.5 text-black focus:border-primary focus-visible:outline-hidden dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+									name="selectDistrict"
+									id="selectDistrict"
+									value={district.toString()}
+									onChange={(e) => handleDistrictSelect(Number(e.target.value))}
+								>
+									{districts.map((district) => {
+										return (
+											<option key={district.id} value={district.id}>
+												{district.name}
+											</option>
+										);
+									})}
+								</select>
 							</div>
-							<div className="flex flex-wrap gap-x-4">
-								<div className="mb-5.5">
-									<select
-										className="rounded border border-stroke bg-white py-3 px-4.5 text-black focus:border-primary focus-visible:outline-hidden dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
-										name="selectDistrict"
-										id="selectDistrict"
-										value={district.toString()}
-										onChange={(e) =>
-											handleDistrictSelect(Number(e.target.value))
-										}
-									>
-										{districts.map((district) => {
-											return (
-												<option key={district.id} value={district.id}>
-													{district.name}
-												</option>
-											);
-										})}
-									</select>
-								</div>
-								<div className="mb-5.5">
-									<select
-										className="rounded border border-stroke bg-white py-3 px-4.5 text-black focus:border-primary focus-visible:outline-hidden dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
-										name="selectCentre"
-										id="selectCentre"
-										value={centre.toString()}
-										onChange={(e) => setCentre(Number(e.target.value))}
-									>
-										{districts
-											.find((d) => d.id === district)
-											?.exam_centres?.map((centre) => (
-												<option key={centre.id} value={centre.id}>
-													{centre.name.length > 32
-														? `${centre.name.substring(0, 30)}...`
-														: centre.name}
-												</option>
-											))}
-									</select>
-								</div>
+							<div className="mb-5.5">
+								<select
+									className="rounded border border-stroke bg-white py-3 px-4.5 text-black focus:border-primary focus-visible:outline-hidden dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+									name="selectCentre"
+									id="selectCentre"
+									value={centre.toString()}
+									onChange={(e) => setCentre(Number(e.target.value))}
+								>
+									{districts
+										.find((d) => d.id === district)
+										?.exam_centres?.map((centre) => (
+											<option key={centre.id} value={centre.id}>
+												{centre.name.length > 32
+													? `${centre.name.substring(0, 30)}...`
+													: centre.name}
+											</option>
+										))}
+								</select>
 							</div>
-							<div className="max-w-full overflow-x-auto">
-								<table className="w-full table-auto">
-									<thead>
-										<tr className="bg-gray-2 dark:bg-meta-4 font-bold text-left">
-											<th className="py-4 px-4 pl-4 xl:pl-11 text-black dark:text-white">
-												Stream
-											</th>
-											<th className="py-4 px-4 text-black dark:text-white">
-												Male
-											</th>
-											<th className="py-4 px-4 text-black dark:text-white">
-												Female
-											</th>
-											<th className="py-4 px-4 text-black dark:text-white">
-												Total
-											</th>
-										</tr>
-									</thead>
-									<tbody>
-										<>
-											{centreStats.counts?.map((count: any, key: number) => (
-												<tr key={key}>
-													<td className="border-b border-[#eee] py-5 px-4 pl-4 dark:border-strokedark xl:pl-11">
-														<h5 className="font-medium text-black dark:text-white">
-															{count.stream_name}
-														</h5>
-													</td>
-													<td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-														<p className="text-black dark:text-white">
-															{count.male_count}
-														</p>
-													</td>
-													<td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-														<p className="text-black dark:text-white">
-															{count.female_count}
-														</p>
-													</td>
-													<td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-														<p className="font-semibold text-black dark:text-white">
-															{count.total_count}
-														</p>
-													</td>
-												</tr>
-											))}
-											<tr className="font-semibold">
-												<td className="border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark xl:pl-11">
-													<h5 className="text-black dark:text-white">
-														<strong>Total</strong>
+						</div>
+						<div className="max-w-full overflow-x-auto">
+							<table className="w-full table-auto">
+								<thead>
+									<tr className="bg-gray-2 dark:bg-meta-4 font-bold text-left">
+										<th className="py-4 px-4 pl-4 xl:pl-11 text-black dark:text-white">
+											Stream
+										</th>
+										<th className="py-4 px-4 text-black dark:text-white">
+											Male
+										</th>
+										<th className="py-4 px-4 text-black dark:text-white">
+											Female
+										</th>
+										<th className="py-4 px-4 text-black dark:text-white">
+											Total
+										</th>
+									</tr>
+								</thead>
+								<tbody>
+									<>
+										{centreStats.counts?.map((count: any, key: number) => (
+											<tr key={key}>
+												<td className="border-b border-[#eee] py-5 px-4 pl-4 dark:border-strokedark xl:pl-11">
+													<h5 className="font-medium text-black dark:text-white">
+														{count.stream_name}
 													</h5>
 												</td>
 												<td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
 													<p className="text-black dark:text-white">
-														{centreStats.total_male}
+														{count.male_count}
 													</p>
 												</td>
 												<td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
 													<p className="text-black dark:text-white">
-														{centreStats.total_female}
+														{count.female_count}
 													</p>
 												</td>
 												<td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-													<p className="font-bold text-primary dark:text-white">
-														<strong>
-															{centreStats.total_male +
-																centreStats.total_female}
-														</strong>
+													<p className="font-semibold text-black dark:text-white">
+														{count.total_count}
 													</p>
 												</td>
 											</tr>
-										</>
-									</tbody>
-								</table>
-							</div>
+										))}
+										<tr className="font-semibold">
+											<td className="border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark xl:pl-11">
+												<h5 className="text-black dark:text-white">
+													<strong>Total</strong>
+												</h5>
+											</td>
+											<td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+												<p className="text-black dark:text-white">
+													{centreStats.total_male}
+												</p>
+											</td>
+											<td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+												<p className="text-black dark:text-white">
+													{centreStats.total_female}
+												</p>
+											</td>
+											<td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+												<p className="font-bold text-primary dark:text-white">
+													<strong>
+														{centreStats.total_male + centreStats.total_female}
+													</strong>
+												</p>
+											</td>
+										</tr>
+									</>
+								</tbody>
+							</table>
 						</div>
 					</div>
 				</>
