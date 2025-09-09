4ac1a57d7de384e81b0531223bab7c0c69c6d52e
Sahithyan <sahithyan2701@gmail.com>
2025-09-10 03:20:29 +0530

    chore: CardDataStats can optionally be a link

--- src/components/CardDataStats.tsx
+++ src/components/CardDataStats.tsx
@@ -1,16 +1,30 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ArrowRight } from "lucide-react";
 import type React from "react";
+import { NavLink } from "react-router-dom";
 
 interface CardDataStatsProps {
 	title: string;
 	total: number;
+	link?: string;
 }
 
-const CardDataStats: React.FC<CardDataStatsProps> = ({ title, total }) => {
-	return (
+const CardDataStats: React.FC<CardDataStatsProps> = ({
+	title,
+	total,
+	link,
+}) => {
+	const content = (
 		<Card className="gap-0 py-4">
 			<CardHeader className="px-4">
-				<CardTitle className="text-sm">{title}</CardTitle>
+				<CardTitle className="text-sm flex justify-between items-center">
+					<span>{title}</span>
+					<span>
+						{link === undefined ? null : (
+							<ArrowRight className="-translate-x-2 group-hover:translate-x-0 transition-transform ease-in-out" />
+						)}
+					</span>
+				</CardTitle>
 			</CardHeader>
 			<CardContent className="px-4">
 				<div className="text-3xl font-semibold tabular-nums @[250px]/card:text-4xl">
@@ -19,6 +33,15 @@ const CardDataStats: React.FC<CardDataStatsProps> = ({ title, total }) => {
 			</CardContent>
 		</Card>
 	);
+
+	if (link) {
+		return (
+			<NavLink to={link} className="group">
+				{content}
+			</NavLink>
+		);
+	}
+	return content;
 };
 
 export default CardDataStats;
