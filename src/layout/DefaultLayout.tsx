8ddcbbb697e43b01d3fe6a5f662025da76826113
Sahithyan <sahithyan2701@gmail.com>
2025-09-10 03:06:31 +0530

    chore: reduce padding

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -8,7 +8,7 @@ export default function DefaultLayout() {
 		<SidebarProvider>
 			<Toaster position="top-right" />
 			<AppSidebar />
-			<main className="flex-[1_0_0] overflow-auto p-4 md:p-6 2xl:p-10">
+			<main className="flex-[1_0_0] overflow-auto p-3 md:p-5 2xl:p-8">
 				<Outlet />
 			</main>
 		</SidebarProvider>
