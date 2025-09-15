0b133fcb0a57cc874c5dad6504a88b3cd42c77c6
Sahithyan <sahithyan2701@gmail.com>
2025-09-15 17:13:31 +0530

    style: run lint-fix

--- src/components/stream-wise-stats.tsx
+++ src/components/stream-wise-stats.tsx
@@ -22,78 +22,61 @@ export default function StreamWiseStats() {
 				toast.error("Failed to fetch stream wise stats.");
 			});
 	}, []);
-	
+
 	return (
 		<section className="mt-8 mb-5">
-				<h2 className="text-xl font-semibold mb-2">
-				Total Stream Wise Stats
-			</h2>
+			<h2 className="text-xl font-semibold mb-2">Total Stream Wise Stats</h2>
 
-				<table className="w-full table-auto">
-					<thead>
-						<tr className="font-bold text-left">
-							<th className="py-4">
-								Stream
-							</th>
-							<th className="py-4">Tamil</th>
-							<th className="py-4">English</th>
-							<th className="py-4">Total</th>
-						</tr>
-					</thead>
-					<tbody>
-						<>
-							{streamStats.counts?.map((count: any, key: number) => (
-								<tr key={key}>
-									<td className="py-5">
-										<h5 className="font-medium">
-											{count.stream_name}
-										</h5>
-									</td>
-									<td className="py-5">
-										<p>
-											{count.tamil_count}
-										</p>
-									</td>
-									<td className="py-5">
-										<p>
-											{count.english_count}
-										</p>
-									</td>
-									<td className="py-5">
-										<p className="font-semibold">
-											{count.total_count}
-										</p>
-									</td>
-								</tr>
-							))}
-							<tr className="font-semibold">
+			<table className="w-full table-auto">
+				<thead>
+					<tr className="font-bold text-left">
+						<th className="py-4">Stream</th>
+						<th className="py-4">Tamil</th>
+						<th className="py-4">English</th>
+						<th className="py-4">Total</th>
+					</tr>
+				</thead>
+				<tbody>
+					<>
+						{streamStats.counts?.map((count: any, key: number) => (
+							<tr key={key}>
 								<td className="py-5">
-									<h5>
-										<strong>Total</strong>
-									</h5>
+									<h5 className="font-medium">{count.stream_name}</h5>
 								</td>
 								<td className="py-5">
-									<p>
-										{streamStats.total_tamil}
-									</p>
+									<p>{count.tamil_count}</p>
 								</td>
 								<td className="py-5">
-									<p>
-										{streamStats.total_english}
-									</p>
+									<p>{count.english_count}</p>
 								</td>
 								<td className="py-5">
-									<p className="font-bold text-primary">
-										<strong>
-											{streamStats.total_tamil + streamStats.total_english}
-										</strong>
-									</p>
+									<p className="font-semibold">{count.total_count}</p>
 								</td>
 							</tr>
-						</>
-					</tbody>
-				</table>
+						))}
+						<tr className="font-semibold">
+							<td className="py-5">
+								<h5>
+									<strong>Total</strong>
+								</h5>
+							</td>
+							<td className="py-5">
+								<p>{streamStats.total_tamil}</p>
+							</td>
+							<td className="py-5">
+								<p>{streamStats.total_english}</p>
+							</td>
+							<td className="py-5">
+								<p className="font-bold text-primary">
+									<strong>
+										{streamStats.total_tamil + streamStats.total_english}
+									</strong>
+								</p>
+							</td>
+						</tr>
+					</>
+				</tbody>
+			</table>
 		</section>
-
 	);
 }
