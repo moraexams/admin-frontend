130b31c1499cbd280b53064b457993526ecf45df
Sahithyan <sahithyan2701@gmail.com>
2025-09-17 20:06:32 +0530

    chore: cleanup attendance sheet download

--- src/services/attendanceSheetService.ts
+++ src/services/attendanceSheetService.ts
@@ -17,27 +17,33 @@ export const generateAttendanceSheetPDFs = async () => {
 };
 
 export const downloadAttendanceSheets = async (subjectId: string) => {
-  try {
-    const response = await axiosInstance.get(`/attendance-sheet/${subjectId}/download`, {
-      responseType: "blob",
-    });
+	try {
+		const response = await axiosInstance.get(
+			`/attendance-sheet/${subjectId}/download`,
+			{
+				responseType: "blob",
+			},
+		);
 
-    const url = window.URL.createObjectURL(new Blob([response.data]));
-    const link = document.createElement("a");
-    link.href = url;
-    link.setAttribute("download", `Mora Exams 2025 - Attendance Sheets - ${subjectId}.zip`);
-    document.body.appendChild(link);
-    link.click();
-    link.remove();
+		const url = window.URL.createObjectURL(new Blob([response.data]));
+		const link = document.createElement("a");
+		link.href = url;
+		link.setAttribute(
+			"download",
+			`Mora Exams 2025 - Attendance Sheets - ${subjectId}.zip`,
+		);
+		document.body.appendChild(link);
+		link.click();
+		link.remove();
 
-    return true;
-  } catch (error) {
-    console.log(error);
-    if (error instanceof AxiosError) {
-      if (error.response?.data) {
-        throw new Error(error.response?.data.message);
-      }
-    }
-    throw "An error occurred while downloading attendance sheet zip file";
-  }
+		return true;
+	} catch (error) {
+		console.log(error);
+		if (error instanceof AxiosError) {
+			if (error.response?.data) {
+				throw new Error(error.response?.data.message);
+			}
+		}
+		throw "An error occurred while downloading attendance sheet zip file";
+	}
 };
